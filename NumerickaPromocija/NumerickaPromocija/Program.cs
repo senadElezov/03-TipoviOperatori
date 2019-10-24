c7c1e1e6b5fa0e303168e710244cd3ca8669d895
tleva <tleva@DESKTOP-J8PKM7S>
2019-10-24 21:15:46 +0200

    Completed

--- NumerickaPromocija/NumerickaPromocija/Program.cs
+++ NumerickaPromocija/NumerickaPromocija/Program.cs
@@ -6,10 +6,13 @@ namespace Vsite.Csharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent. Dijeljitelj i dijeljenik odabrati tako da dijeljenje bude s ostatkom.
-
-            // TODO:041 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
-
+          
+            int a = 3;
+            int b = 2;
+            Console.WriteLine(a / b);
+            
+            double c = 2.3;
+            Console.WriteLine(c + a);
             Console.ReadKey();
         }
     }
